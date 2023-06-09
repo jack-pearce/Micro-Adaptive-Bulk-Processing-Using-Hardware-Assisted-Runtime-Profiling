7d9ca6f18fc9483add2bf4247979544c5cb8e58b
Jack Pearce <jack.pearce4@btinternet.com>
2023-06-09 17:36:36 +0100

    Measurement of select with PAPI

--- src/benchmarking/selectTests.h
+++ src/benchmarking/selectTests.h
@@ -4,4 +4,6 @@
 void selectBranchTest_1();
 void selectPredicationTest_1();
 
+int BM_selectSetup(const std::string& filePath, std::vector<int>& inputData, std::vector<int>& selection);
+
 #endif //MICRO_ADAPTIVE_BULK_PROCESSING_LIBRARY_SELECTBRANCHTESTS_H
--- src/perf_counters/hpcTests.cpp
+++ src/perf_counters/hpcTests.cpp
@@ -1,37 +1,63 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
+#include "../data_generation/data_generator.h"
+#include "../library/select.h"
+#include "../benchmarking/selectTests.h"
+#include "../utils/dataHelpers.h"
 #include "../../libs/papi/src/install/include/papi.h"
 
 
-void hpcTest_1() {}
+int initialisePAPIandCreateEventSet(std::vector<std::string>& hpcs) {
+    int retval, EventCode, EventSet=PAPI_NULL;
 
-void hpcTest_2() {
-        long_long start_cycles, end_cycles, start_usec, end_usec;
-        int EventSet = PAPI_NULL;
-
-        if (PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT)
+/* Initialize the PAPI library */
+    retval = PAPI_library_init(PAPI_VER_CURRENT);
+    if (retval != PAPI_VER_CURRENT) {
+        std::cerr << "PAPI library init error!" << std::endl;
         exit(1);
+    }
 
-/* Gets the starting time in clock cycles */
-        start_cycles = PAPI_get_real_cyc();
-
-/* Gets the starting time in microseconds */
-        start_usec = PAPI_get_real_usec();
-
-/*Create an EventSet */
-        if (PAPI_create_eventset(&EventSet) != PAPI_OK)
+/* Create the Event Set */
+    if (PAPI_create_eventset(&EventSet) != PAPI_OK) {
+        std::cerr << "PAPI could not create event set!" << std::endl;
         exit(1);
-/* Gets the ending time in clock cycles */
-        end_cycles = PAPI_get_real_cyc();
+    }
 
-/* Gets the ending time in microseconds */
-        end_usec = PAPI_get_real_usec();
+    for (const std::string& hpc : hpcs) {
+        /* Get event code */
+        retval = PAPI_event_name_to_code(hpc.c_str(), &EventCode);
+        if (retval != PAPI_OK) {
+            std::cerr << "PAPI could not create event code!" << std::endl;
+            exit(1);
+        }
 
-        printf("Wall clock cycles: %lld\n", end_cycles - start_cycles);
-        printf("Wall clock time in microseconds: %lld\n", end_usec - start_usec);
+        /* Add Events to our Event Set */
+        retval = PAPI_add_event(EventSet, EventCode);
+        if (retval != PAPI_OK) {
+            std::cerr << "Could not add '" << hpc << "' to event set!" << std::endl;
+            std::cerr << "Error code: " << retval << std::endl;
+            exit(1);
+        }
+    }
+
+    return EventSet;
 }
 
-void hpcTest_3() {
+void teardownPAPI(int eventSet) {
+    PAPI_destroy_eventset(&eventSet);
+    PAPI_shutdown();
+}
+
+void printHpcValues(std::vector<std::string>& hpcs, long_long hpcValues[]) {
+    int n = hpcs.size();
+    for (int i = 0; i < n; ++i) {
+        std::cout << hpcs[i] << ": " << hpcValues[i] << std::endl;
+    }
+}
+
+void hpcTest_1() {
     int retval, EventSet=PAPI_NULL;
     long_long values[1];
 
@@ -94,4 +120,82 @@ void hpcTest_3() {
         exit(1);
 
     printf("After stopping the counters: %lld\n",values[0]);
+
+    // Cleanup
+    PAPI_destroy_eventset(&EventSet);
+    PAPI_shutdown();
+}
+
+void hpcTest_2() {
+        long_long start_cycles, end_cycles, start_usec, end_usec;
+        int EventSet = PAPI_NULL;
+
+        if (PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT)
+        exit(1);
+
+/* Gets the starting time in clock cycles */
+        start_cycles = PAPI_get_real_cyc();
+
+/* Gets the starting time in microseconds */
+        start_usec = PAPI_get_real_usec();
+
+/*Create an EventSet */
+        if (PAPI_create_eventset(&EventSet) != PAPI_OK)
+        exit(1);
+/* Gets the ending time in clock cycles */
+        end_cycles = PAPI_get_real_cyc();
+
+/* Gets the ending time in microseconds */
+        end_usec = PAPI_get_real_usec();
+
+        printf("Wall clock cycles: %lld\n", end_cycles - start_cycles);
+        printf("Wall clock time in microseconds: %lld\n", end_usec - start_usec);
+}
+
+void hpcTest_3() {
+    std::string filePath = "/home/jack/CLionProjects/micro-adaptive-bulk-processing-library/data/uniformIntDistribution.csv";
+    int numElements = 4000000 / sizeof(int);
+    generateUniformDistributionCSV(filePath, numElements);
+
+    std::vector<int> inputData;
+    std::vector<int> selection;
+    int elements = BM_selectSetup(filePath, inputData, selection);
+
+    std::vector<std::string> hpcs = {"INSTRUCTION_RETIRED",
+                                     "BRANCH_INSTRUCTIONS_RETIRED",
+                                     "MISPREDICTED_BRANCH_RETIRED"};
+    long_long hpcValues[hpcs.size()];
+    int EventSet = initialisePAPIandCreateEventSet(hpcs);
+
+    if (PAPI_start(EventSet) != PAPI_OK) {
+        std::cerr << "PAPI could not start counting events!" << std::endl;
+        exit(1);
+    }
+
+    selectBranch(elements, inputData, selection, 50);
+
+    if (PAPI_stop(EventSet, hpcValues) != PAPI_OK) {
+        std::cerr << "PAPI could not stop counting events!" << std::endl;
+        exit(1);
+    }
+
+    printHpcValues(hpcs, hpcValues);
+
+    teardownPAPI(EventSet);
+
+
+
+
+    /* Read the counting events in the Event Set */
+//    if (PAPI_read(EventSet, values) != PAPI_OK)
+//        exit(1);
+//
+//    printf("After reading the counters: %lld\n",values[0]);
+
+/* Reset the counting events in the Event Set */
+//    if (PAPI_reset(EventSet) != PAPI_OK)
+//        exit(1);
+
+
+
 }
