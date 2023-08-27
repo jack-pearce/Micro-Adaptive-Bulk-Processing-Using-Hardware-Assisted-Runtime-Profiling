ab1406a89dc15f5d9322b9e7f64e6f2ac82ddab6
Jack Pearce <jack.pearce4@btinternet.com>
2023-08-27 16:56:25 +0100

    Server test

--- src/library/operators/radixPartitionImplementation.h
+++ src/library/operators/radixPartitionImplementation.h
@@ -13,7 +13,7 @@ inline void radixPartitionAux(int start, int end, T *keys, T *buffer,
                               int mask, int numBuckets, std::vector<int> &buckets, int msbPosition, int &radixBits) {
     int i;
 //    int shifts = msbPosition - radixBits;
-    int shifts = 4;
+    int shifts = 14;
 
     for (i = start; i < end; i++) {
         buckets[1 + ((keys[i] >> shifts) & mask)]++;
