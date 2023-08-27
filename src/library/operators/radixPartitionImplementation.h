44fcd9ce9ce8be7f6a15115dd62a18c583bb2720
Jack Pearce <jack.pearce4@btinternet.com>
2023-08-27 17:16:32 +0100

    Server test

--- src/library/operators/radixPartitionImplementation.h
+++ src/library/operators/radixPartitionImplementation.h
@@ -12,8 +12,8 @@ template<typename T>
 inline void radixPartitionAux(int start, int end, T *keys, T *buffer,
                               int mask, int numBuckets, std::vector<int> &buckets, int msbPosition, int &radixBits) {
     int i;
-//    int shifts = msbPosition - radixBits;
-    int shifts = 27 - radixBits;
+    int shifts = msbPosition - radixBits;
+//    int shifts = 27 - radixBits;
 //    int shifts = 16;
 
     for (i = start; i < end; i++) {
@@ -75,6 +75,7 @@ void radixPartition(int n, T *keys, int radixBits) {
         largest >>= 1;
         msbPosition++;
     }
+    std::cout << msbPosition << std::endl;
 
     std::vector<int> buckets(1 + numBuckets, 0);
     T *buffer = new T[n];
