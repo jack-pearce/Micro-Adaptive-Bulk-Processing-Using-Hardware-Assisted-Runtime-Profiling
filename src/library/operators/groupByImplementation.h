77d886a684eff2871891e87587616d3b5b513e65
Jack Pearce <jack.pearce4@btinternet.com>
2023-09-01 15:16:20 +0100

    Server test

--- src/library/operators/groupByImplementation.h
+++ src/library/operators/groupByImplementation.h
@@ -349,6 +349,7 @@ vectorOfPairs<T1, T2> groupByAdaptive(int n, T1 *inputGroupBy, T2 *inputAggregat
         Counters::getInstance().readSharedEventSet();
 
         if ((static_cast<float>(tuplesToProcess) / counterValues[0]) < tuplesPerLastLevelCacheMissThreshold) {
+            std::cout << "Switched to sort at index " << index << std::endl;
             tuplesToProcess = std::min(tuplesBetweenHashing, n - index);
 
             sectionsToBeSorted.emplace_back(index, index + tuplesToProcess);
