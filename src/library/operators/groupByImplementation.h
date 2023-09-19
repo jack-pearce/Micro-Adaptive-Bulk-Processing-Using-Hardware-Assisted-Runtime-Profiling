b515eba88e1a37529893b0288eef667d61301cd6
Jack Pearce <jack.pearce4@btinternet.com>
2023-09-19 19:17:34 +0100

    Revert change

--- src/library/operators/groupByImplementation.h
+++ src/library/operators/groupByImplementation.h
@@ -72,8 +72,7 @@ vectorOfPairs<T1, T2> groupByHash(int n, T1 *inputGroupBy, T2 *inputAggregate, i
     static_assert(std::is_integral<T1>::value, "GroupBy column must be an integer type");
     static_assert(std::is_arithmetic<T2>::value, "Payload column must be an numeric type");
 
-//    tsl::robin_map<T1, T2> map(std::max(static_cast<int>(2.5 * cardinality), 400000));
-    tsl::robin_map<T1, T2> map;
+    tsl::robin_map<T1, T2> map(std::max(static_cast<int>(2.5 * cardinality), 400000));
     int index = 0;
 
 //    int tuplesToProcess;
@@ -324,8 +323,7 @@ vectorOfPairs<T1, T2> groupByAdaptive(int n, T1 *inputGroupBy, T2 *inputAggregat
     constexpr int tuplesBetweenHashing = 2*1000*1000;
     int initialSize = std::max(static_cast<int>(2.5 * cardinality), 400000);
 
-//    tsl::robin_map<T1, T2> map(initialSize);
-    tsl::robin_map<T1, T2> map;
+    tsl::robin_map<T1, T2> map(initialSize);
     typename tsl::robin_map<T1, T2>::iterator it;
 
     std::vector<std::string> counters = {"PERF_COUNT_HW_CACHE_MISSES"};
